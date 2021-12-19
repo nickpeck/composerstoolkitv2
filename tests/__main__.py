0c2ff66499413eef9bae436d2492fd5d07dbebe8
Nick Peck <sysadmin@take1.tv>
2021-12-18 19:42:57 -0800

    initial commit

---
+++ tests/__main__.py
@@ -0,0 +1,19 @@
+# import unittest
+
+# from . import (testmodule)
+
+# def suite():
+    # test_suite = unittest.TestSuite()
+    # test_suite.addTests(
+        # unittest.TestLoader().loadTestsFromModule(testmodule))
+    # return test_suite
+
+# if __name__ == '__main__':
+    # suite = suite()
+    # runner=unittest.TextTestRunner()
+    # outcome = runner.run(suite)
+    # print(outcome)
+    # was_successful = outcome.wasSuccessful()
+    # if was_successful:
+        # exit(0)
+    # exit(1)
---
+++ tests/testmodule.py
@@ -0,0 +1,9 @@
+import unittest
+
+class TestSuite(unittest.TestCase):
+
+    def test_case(self):
+        assert True == False
+
+if __name__ == "__main__":
+    unittest.main()
