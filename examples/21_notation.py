c75462ecdb94eb92fe4174399ca444787cb70092
nickpeck <nicholas.w.peck@gmail.com>
2022-03-19 19:59:42 -0700

    adds example of notation

---
+++ examples/21_notation.py
@@ -0,0 +1,21 @@
+from composerstoolkit import *
+
+artificial_scale = Sequence.from_generator(
+    resultant_pitches(counters=[3,4,9], start_at=50)).bake()
+
+chords = Sequence.from_generator(artificial_scale)\
+    .transform(aggregate(4, HALF_NOTE))
+
+seq = Sequence(
+    events=Permutations([
+        Event(pitches=[48], duration=QUARTER_NOTE),
+        Event(pitches=[43], duration=QUARTER_NOTE),
+        Event(pitches=[], duration=QUARTER_NOTE),
+        Event(pitches=[41], duration=QUARTER_NOTE)]).flatten()
+    ).transform(transpose(24))
+
+Container(bpm=100, playback_rate=1)\
+    .add_sequence(seq.bake())\
+    .add_sequence(chords.bake())\
+    .show_notation()\
+    .playback()
