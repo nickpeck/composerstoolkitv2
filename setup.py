107485fcffbdfd0a049773683f5d37d224cdfb4b
nickpeck <nicholas.w.peck@gmail.com>
2024-04-29 09:00:38 -0700

    add setup.py

---
+++ setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+
+setuptools.setup(
+    name='composerstoolkit',
+    version='0.2.0',
+    license='LICENSE',
+    author='nickpeck',
+    author_email='',
+    description='',
+    keywords=[],
+    url='',
+    package_dir={'': 'src'},
+    packages=setuptools.find_packages(where='src'),
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Programming Language :: Python :: 3.10',
+        'License :: OSI Approved :: MIT License',
+    ],
+    python_requires='>=3.7',
+    include_package_data=True,
+)
