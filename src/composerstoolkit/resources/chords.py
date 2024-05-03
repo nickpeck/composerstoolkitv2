1e8ea6a047f46d92e0f7f654a29da5db4c29da35
nickpeck <nicholas.w.peck@gmail.com>
2024-05-02 18:05:34 -0700

    Add sharps and flats to ChordBuilder

--- src/composerstoolkit/resources/chords.py
+++ src/composerstoolkit/resources/chords.py
@@ -17,11 +17,21 @@ class ChordBuilder:
     """
     notes = {
         "C" : 0,
+        "C#" : 1,
+        "Db": 1,
         "D" : 2,
+        "D#" : 3,
+        "Eb" : 3,
         "E" : 4,
         "F" : 5,
+        "F#": 6,
+        "Gb" : 6,
         "G" : 7,
+        "G#" : 8,
+        "Ab": 8,
         "A" : 9,
+        "A#": 10,
+        "Bb": 10,
         "B" : 11
     }
 
@@ -31,7 +41,7 @@ class ChordBuilder:
         should be placed in.
         """
         self.schema = re.compile(
-            "^([A-G])(maj|min|minmaj|dim|sus|aug)?([1-9]*)"\
+            "^([A-G][#b]*)(maj|min|minmaj|dim|sus|aug)?([1-9]*)"\
             +"(aug[1-9]*|b[1-9]*|#[1-9]*)?"\
             +"(aug[1-9]*|b[1-9]*|#[1-9]*)?"\
             +"(\/[A-G])?$",
