5d0ce8897aceef9bb75ea5580663a757de43f941
nickpeck <nicholas.w.peck@gmail.com>
2024-06-27 00:18:55 -0700

    some work in progress improving the scales to include church modes

--- src/composerstoolkit/resources/pitches.py
+++ src/composerstoolkit/resources/pitches.py
@@ -18,6 +18,27 @@ pitch(69)
 import math
 import re
 
+PITCH_CLASSES = {
+    "C": 0,
+    "C#": 1,
+    "Db": 1,
+    "D": 2,
+    "D#": 3,
+    "Eb": 3,
+    "E": 4,
+    "F": 5,
+    "F#": 6,
+    "Gb": 6,
+    "G": 7,
+    "G#": 8,
+    "Ab": 8,
+    "A": 9,
+    "A#": 10,
+    "Bb": 10,
+    "B": 11,
+}
+
+
 class PitchFactory:
     notes = {
         "C" : 0,
--- src/composerstoolkit/resources/scales.py
+++ src/composerstoolkit/resources/scales.py
@@ -1,11 +1,19 @@
 """Library functions for defining common scale types
 """
 from . import NOTE_MAX
+from . pitches import PITCH_CLASSES
 
-MAJ_SCALE_PITCH_CLASSES = {0,2,4,5,7,9,11}
-MEL_MINOR_SCALE_PITCH_CLASSES = {0,2,3,5,7,9,11}
+MAJ = IONIAN = IONIAN_MODE_PITCH_CLASSES = MAJ_SCALE_PITCH_CLASSES = {0,2,4,5,7,9,11}
+MEL_MINOR = MEL_MINOR_SCALE_PITCH_CLASSES = {0,2,3,5,7,9,11}
 HAR_MINOR_SCALE_PITCH_CLASSES = {0,2,3,5,7,8,11}
 HAR_MAJOR_SCALE_PITCH_CLASSES = {0,2,4,5,7,8,11}
+# Church Modes
+DORIAN = DORIAN_MODE_PITCH_CLASSES = {0,2,3,5,7,9,10}
+PHYGIAN = PHYGIAN_MODE_PITCH_CLASSES = {0,1,3,5,7,8,10}
+LYDIAN = LYDIAN_MODE_PITCH_CLASSES = {0,2,4,6,7,9,11}
+MIXOLYDIAN = _MODE_PITCH_CLASSES = {0,2,3,5,7,9,11}
+AEOLIAN = AEOLIAN_MODE_PITCH_CLASSES = {0,2,3,5,7,8,10}
+LOCRIAN = LOCRIAN_MODE_PITCH_CLASSES = {0,1,3,5,6,8,10}
 # these are Messiaen's modes of limited transposition
 MODE_1_PITCH_CLASSES = {0,2,4,6,8,10}
 MODE_2_PITCH_CLASSES = {0,1,3,4,6,7,9,10}
@@ -20,6 +28,8 @@ OCTATONIC_SCALE_PITCH_CLASSES = MODE_2_PITCH_CLASSES
 DIMINISHED_SCALE_PITCH_CLASSES = MODE_2_PITCH_CLASSES
 
 CHROMATIC_SCALE_PITCH_CLASSES = {*range(0,11)}
+
+# TODO refactor these to use the mode() function below rather than duplicating (need to update examples too)
 
 chromatic = {*range(0, NOTE_MAX)}
 C_major = set([0] + sorted(filter(lambda x: (x % 12) in \
@@ -77,3 +87,9 @@ Ab_har_major = set(map(lambda x: x+8, C_har_major))
 A_har_major = set(map(lambda x: x+9, C_har_major))
 Bb_har_major = set(map(lambda x: x+10, C_har_major))
 B_har_major = set(map(lambda x: x+11, C_har_major))
+
+def mode(root="C", mode=DORIAN):
+    transposition = PITCH_CLASSES[root.upper()]
+    c_mode = [0] + sorted(filter(lambda x: (x % 12) in \
+        mode , range(1, NOTE_MAX)))
+    return set(map(lambda x: x+transposition, c_mode))
